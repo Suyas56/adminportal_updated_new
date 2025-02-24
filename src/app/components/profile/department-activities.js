403749981fddaf771452be41fc102de8f39a0a9a
suyash35565 <suyashv.ug23.cs@nitp.ac.in>
2025-02-24 08:36:12 +0530

    corrected date offset issue and made start_date & end_date required fields

--- src/app/components/profile/department-activities.js
+++ src/app/components/profile/department-activities.js
@@ -66,7 +66,17 @@ export const AddForm = ({ handleClose, modal }) => {
     const handleSubmit = async (e) => {
         setSubmitting(true)
         e.preventDefault()
+        if (!content.start_date) {
+            alert('Start date is required');
+            setSubmitting(false);
+            return;
+        }
 
+        if (!content.end_date && content.end_date !== "Continue") {
+            alert('End date or "Continue" must be selected');
+            setSubmitting(false);
+            return;
+        }
         try {
             const formattedStartDate = format(content.start_date, 'yyyy-MM-dd');
             const formattedEndDate = content.end_date ? format(content.end_date, 'yyyy-MM-dd') : null;
@@ -206,7 +216,7 @@ export const EditForm = ({ handleClose, modal, values }) => {
     const handleSubmit = async (e) => {
         setSubmitting(true);
         e.preventDefault();
-    
+       
         try {
             const formatToISODate = (date) => {
                 if (!date) return null;
